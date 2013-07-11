53c78f0343ad26b08987f5e146abae0750b3e509
Maksim Berjoza <torbjon@gmail.com>
2013-07-11 12:47:51 +0300

    small refactoring

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'sinatra'
 require 'dzintars/kompilators'
 
@@ -11,8 +13,15 @@ end
 
 post '/' do
   $stdout = StringIO.new
-  code = params[:code].gsub('`', '\'')
-  code = 'puts \'ej dirst\'' if code.include?('%x{')
+  code = code_formatting(params[:code])
   eval Dzintars::Kompilators.new.uz_ruby(code)
   $stdout.string
 end
+
+private
+
+  def code_formatting(code)
+    code = code.gsub('`', '\'')
+    code = 'puts \'Lūdzu tā nedariet, man sāp\'' if code.include?('%x{')
+    code
+  end
